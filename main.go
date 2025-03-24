9d382bf2b92c45c7e8f9f35adde3c49ecf3e7b7a
Wang Yaoshen <wangyaoshen@zhonghegene.com>
2025-03-24 14:44:46 +0800

    🎨add LoadCarrierList

--- main.go
+++ main.go
@@ -39,46 +39,36 @@ var (
 		"载体",
 	}
 
-	title2 = []string{
+	CarrierListTitle = []string{
 		"载体名称",
 		"载体序列",
 	}
+	title3 = []string{
+		"质粒名称",
+		"序列",
+	}
+
+	CreateSheet = "构建完成"
 )
 
 func main() {
 	flag.Parse()
-	if *input == "" || *output == "" {
+	if *input == "" {
 		flag.Usage()
 		log.Fatal("-i required")
 	}
-
-	xlsx := simpleUtil.HandleError(excelize.OpenFile(*input))
-	// name -> seq
-	carrierInfo := make(map[string]string)
-	slice := simpleUtil.HandleError(xlsx.GetRows("载体清单"))
-	for i := range slice {
-		row := slice[i]
-		if i == 0 {
-			if len(row) < len(title1) {
-				log.Fatalf("title leak column:[%+v]", row)
-			}
-			for i := range title2 {
-				if row[i] != title2[i] {
-					log.Fatalf("title1 error:%d[%s]vs[%s]", i+1, row[i], title2[i])
-				}
-			}
-			continue
-		}
-		data := make(map[string]string)
-		for j := range title1 {
-			data[title1[j]] = row[j]
-		}
-		carrierInfo[data["载体名称"]] = data["载体序列"]
+	if *output == "" {
+		*output = strings.TrimSuffix(*input, "xlsx") + "构建完成"
 	}
 
-	out := osUtil.Create(*output + ".fasta")
-	defer simpleUtil.DeferClose(out)
-	slice = simpleUtil.HandleError(xlsx.GetRows("Sheet1"))
+	xlsx := simpleUtil.HandleError(excelize.OpenFile(*input))
+	carrierInfo := LoadCarrierList(xlsx, "载体清单", CarrierListTitle)
+
+	var FA = osUtil.Create(*output + ".fasta")
+	defer simpleUtil.DeferClose(FA)
+	xlsx.NewSheet(CreateSheet)
+
+	slice := simpleUtil.HandleError(xlsx.GetRows("Sheet1"))
 	for i := range slice {
 		row := slice[i]
 		if i == 0 {
@@ -136,6 +126,29 @@ func main() {
 			log.Fatalf("酶切位置找不到:[%d,%d],[%d,%d]", e1start, e1end, e2start, e2end)
 		}
 
-		fmtUtil.Fprintf(out, ">%s-%s-%s\n%s\n", data["基因内名"], data["基因名称"], data["载体"], data["图谱"])
+		fmtUtil.Fprintf(FA, ">%s-%s-%s\n%s\n", data["基因内名"], data["基因名称"], data["载体"], data["图谱"])
 	}
+}
+
+// 读取载体清单 name -> seq
+func LoadCarrierList(xlsx *excelize.File, sheet string, title []string) map[string]string {
+	carrierInfo := make(map[string]string)
+	slice := simpleUtil.HandleError(xlsx.GetRows(sheet))
+	for i := range slice {
+		row := slice[i]
+		if i == 0 { // 校验表头
+			if len(row) < len(title) {
+				log.Fatalf("title leak column:[%+v]", row)
+			}
+			for i := range title {
+				if row[i] != title[i] {
+					log.Fatalf("title1 error:%d[%s]vs[%s]", i+1, row[i], title[i])
+				}
+			}
+			continue
+		}
+		carrierInfo[row[0]] = row[1]
+	}
+	return carrierInfo
+
 }
